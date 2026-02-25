11396c68e39e852f78d38819c8a78d1c3c20f296
gassraphael <gassraphael@proton.me>
2026-02-25 16:42:52 +0400

    AlphaPEM can now be exported

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# Package export for AlphaPEM project
+# Allow importing AlphaPEM as `import AlphaPEM` or `import alphapem` depending on sys.path
+from .model import AlphaPEM
+
+__all__ = ["AlphaPEM"]
+
---
+++ model/__init__.py
@@ -0,0 +1,5 @@
+# Expose AlphaPEM class at model package level
+from .AlphaPEM import AlphaPEM
+
+__all__ = ["AlphaPEM"]
+
