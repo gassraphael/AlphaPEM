1257c15343fb1cb3b7db1170526027c92d0fc7bd
gassraphael <gassraphael@proton.me>
2026-06-22 15:53:04 +0200

    fix(calibration): parameters of the Evolutionary algorithm are better considered

--- examples/run_calibration.jl
+++ examples/run_calibration.jl
@@ -63,7 +63,7 @@ calibration_conditions = [
 # ── Genetic Algorithm settings ────────────────────────────────────────────────
 
 ga_cfg = GAConfig(
-    num_generations = 10,                     # 1000+ recommended for high precision
+    num_generations = 10,                       # 1000+ recommended for high precision
     pop_size        = 128,                      # 128+ recommended
     target_error    = 0.5/100,                  # Stop if RMSE < 0.5%
 )
