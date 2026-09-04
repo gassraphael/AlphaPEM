6bd40c1dec4f80040c56aca54cb14c23529e579c
gassraphael <gassraphael@proton.me>
2026-09-04 14:38:29 +0400

    refactor(calibration settings)

--- examples/run_calibration.jl
+++ examples/run_calibration.jl
@@ -75,8 +75,8 @@ calibration_conditions = [
 # ── Genetic Algorithm settings ────────────────────────────────────────────────
 
 ga_cfg = GAConfig(
-    num_generations = 2000,                     # minimum of 500 generation, 1000+ recommended for high precision
-    pop_size        = 256,                      # 128+ recommended
+    num_generations = 500,                      # minimum of 500 generation, 1000+ recommended for high precision
+    pop_size        = 128,                      # 128+ recommended
     target_error    = 1/100,                    # Stop if RMSE < 1%
 )
 
